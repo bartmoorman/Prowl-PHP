175c47de830fca763f4915a207b181a031f13407
iVirus <ivirus@id10t.us>
2011-07-29 09:22:10 -0600

    initial commit

---
+++ example.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php
+<?php
+require_once('prowl.php');
+
+$prowl = new Prowl();
+
+$prowl->addApiKey('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+$prowl->addApiKey('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy');
+$prowl->setApplication('My Application');
+$prowl->setEvent('New Event');
+$prowl->setDescription('Notification Text');
+$prowl->setUrl('http://www.google.com/');
+$prowl->setPriority(-1);
+
+$prowl->send();
+?>
